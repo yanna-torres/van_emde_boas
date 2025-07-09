68b1c59db7567c137a209b5187526ea0fbdfab72
Yanna Torres <torres.yanna03@gmail.com>
2025-07-08 21:02:56 -0300

    Move main application logic to simple.py and remove it from app.py

--- app.py
+++ app.py
@@ -1,26 +1,2 @@
-from clrs_20.simple import WithBTree
-
 if __name__ == "__main__":
     print("This is the main application file.")
-
-    tree = WithBTree(16)
-
-    tree.insert(2)
-    tree.insert(3)
-    tree.insert(4)
-    tree.insert(5)
-    tree.insert(7)
-    tree.insert(14)
-    tree.insert(15)
-
-    print(tree)
-
-    print("Member 2:", tree.member(2))
-
-    print(tree.root_value())
-
-    print("Minimum:", tree.minimum())
-    print("Maximum:", tree.maximum())
-
-    print("Successor of 14:", tree.successor(14))
-    print("Predecessor of 14:", tree.predecessor(14))
--- clrs_20/simple.py
+++ clrs_20/simple.py
@@ -154,3 +154,27 @@ class WithBTree:
                 return i - self.leaf_start
             i = parent
         return None
+
+
+if __name__ == "__main__":
+    tree = WithBTree(16)
+
+    tree.insert(2)
+    tree.insert(3)
+    tree.insert(4)
+    tree.insert(5)
+    tree.insert(7)
+    tree.insert(14)
+    tree.insert(15)
+
+    print(tree)
+
+    print("Member 2:", tree.member(2))
+
+    print(tree.root_value())
+
+    print("Minimum:", tree.minimum())
+    print("Maximum:", tree.maximum())
+
+    print("Successor of 14:", tree.successor(14))
+    print("Predecessor of 14:", tree.predecessor(14))
